37098e68f67caa9df74c0256e52f42b7b78d8380
MasterVChicken <yanliang.l@outlook.com>
2024-07-03 11:26:56 -0500

    Add extractor for iso-surface in split data blocks

---
+++ extractor/IsosurfaceExtractor.h
@@ -0,0 +1,41 @@
+//
+// Created by Yanliang Li on 7/2/24.
+//
+
+#ifndef VTK_TRY_ISOSURFACEEXTRACTOR_H
+#define VTK_TRY_ISOSURFACEEXTRACTOR_H
+
+#include <vtkm/filter/contour/ContourFlyingEdges.h>
+#include <vtkm/cont/DataSet.h>
+
+struct IsoSurfaceResult {
+    vtkm::Float32 isovalue;
+    std::vector<size_t> blockIds;
+};
+
+std::vector<IsoSurfaceResult>
+processIsovalues(const std::vector<vtkm::cont::DataSet> &dataSets, const std::vector<vtkm::Float32> &isovalues) {
+    std::vector<IsoSurfaceResult> results;
+
+    for (auto isovalue: isovalues) {
+        IsoSurfaceResult result;
+        result.isovalue = isovalue;
+
+        for (size_t i = 0; i < dataSets.size(); ++i) {
+            vtkm::filter::contour::ContourFlyingEdges filter;
+            filter.SetIsoValue(isovalue);
+            filter.SetActiveField("data");
+            auto outputDataSet = filter.Execute(dataSets[i]);
+
+            if (!outputDataSet.GetCellSet().GetNumberOfCells() > 0) {
+                result.blockIds.push_back(i);
+            }
+        }
+
+        results.push_back(result);
+    }
+
+    return results;
+}
+
+#endif //VTK_TRY_ISOSURFACEEXTRACTOR_H
