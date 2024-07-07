ede476a2830bbf813d0ce166bc625f037ab511ea
MasterVChicken <yanliang.l@outlook.com>
2024-07-07 13:32:23 -0500

    Add compression module for ZFP.

--- test/test_compression_zfp.cpp
+++ test/test_compression_zfp.cpp
@@ -7,10 +7,40 @@
 #include "../extractor/IsosurfaceExtractor.h"
 #include <vtkm/cont/Initialize.h>
 #include <iostream>
+#include <zfp.h>
+
+// Compress with ZFP
+std::vector<uint8_t> compressDataWithZFP(const std::vector<float>& data, size_t nx, size_t ny, size_t nz, double tolerance) {
+    zfp_type type = zfp_type_float;
+    zfp_field* field = zfp_field_3d(const_cast<float*>(data.data()), type, nx, ny, nz);
+
+    // set parameter
+    zfp_stream* zfp = zfp_stream_open(nullptr);
+    zfp_stream_set_accuracy(zfp, tolerance);
+
+    size_t bufsize = zfp_stream_maximum_size(zfp, field);
+    std::vector<uint8_t> buffer(bufsize);
+
+    bitstream* stream = stream_open(buffer.data(), bufsize);
+    zfp_stream_set_bit_stream(zfp, stream);
+    zfp_stream_rewind(zfp);
+
+    size_t compressedSize = zfp_compress(zfp, field);
+    if (compressedSize == 0) {
+        std::cerr << "Compression failed!" << std::endl;
+    }
+
+    zfp_field_free(field);
+    zfp_stream_close(zfp);
+    stream_close(stream);
+
+    buffer.resize(compressedSize);
+    return buffer;
+}
 
 int main(int argc, char* argv[]) {
 
-    double error_bound = 0.01;
+    double tolerance = 0.01;
     vtkm::cont::InitializeOptions options = vtkm::cont::InitializeOptions::RequireDevice | vtkm::cont::InitializeOptions::AddHelp;
     vtkm::cont::Initialize(argc, argv, options);
 
